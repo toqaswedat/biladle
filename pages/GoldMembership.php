6c9b29d26074429aac4182c5d0b4634e2dcfbb4e
toqaqswedat <toqa.j.swedat@gmail.com>
2022-11-06 17:00:03 +0200

    Gold Membership page

---
+++ pages/GoldMembership.php
@@ -0,0 +1,10 @@
+<?php include('navbar.php');?>
+<div class="text-right p-5">
+    <p>الصفحة الرئيسية > خطة الدفع > العضوية الذهبية</p>
+    <h1>العضوية الذهبية</h1>
+</div>
+
+
+
+
+<?php include('footer.php');?>
