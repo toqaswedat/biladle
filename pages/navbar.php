ca36d8c0cc793c04e9a90dccb7ffb8709ff67446
toqaqswedat <toqa.j.swedat@gmail.com>
2022-11-07 16:53:45 +0200

    Pages

--- pages/navbar.php
+++ pages/navbar.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="../css/footer.css" type="text/css">
     <link rel="stylesheet" href="../css/cyber.css" type="text/css">
     <link rel="stylesheet" href="../css/contactus.css" type="text/css">
+    <link rel="stylesheet" href="../css/member.css" type="text/css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
---
+++ pages/services.php
@@ -0,0 +1,157 @@
+<?php include('navbar.php');?>
+<section class="d-flex flex-column justify-content-center align-items-center text-light" style="background-image:url('../image/coveraboutUs.svg');  background-size: cover; background-repeat: no-repeat; height:374px;">
+<h6>الصفحة الرئيسية >  خدماتنا</h6>
+<h1>خدماتنا</h1>
+</section>
+
+<section class="pt-5 pb-5 serverHome">
+  <h1 class="text-center custom-control"><u class="pt-2 undellineHome">خدم</u>ات بالعدل </h1>
+  <p class="d-flex flex-column justify-content-center align-items-center text-center pt-5">تقدم بالعدل المشورة القانونية والتمثيل القانوني لك ولعائلتك بما في ذلك زوجك أو أطفالك<br> الذين تقل أعمارهم عن 18 عامًا. يمكنك حماية حقوق عائلتك القانونية من خلال خيارات العضوية <br>الذهبية أو الذهبية أو البلاتينية. </p>
+
+
+  <div class=" mr-5 ml-5 pt-5 d-flex servisHomeSection" >
+    <div class="col-sm-3 " >    
+      <img  class='' src="../image/civil.svg" />
+    </div>
+    <div class="col-sm-3" >    
+      <img  class='' src="../image/criminal.svg" />
+    </div>
+    <div class="col-sm-3" >    
+      <img  class='' src="../image/workissues.svg" />
+    </div>
+    <div class="col-sm-3" >    
+      <img  class='' src="../image/family.svg" />
+    </div>
+  </div>
+  <div class=" mr-5 ml-5 pt-5 d-flex servisHomeSection" >
+    <div class="col-sm-3 " >    
+      <img  class='' src="../image/court.svg" />
+    </div>
+    <div class="col-sm-3" >    
+      <img  class='' src="../image/dept.svg" />
+    </div>
+    <div class="col-sm-3" >    
+      <img  class='' src="../image/advices.svg" />
+    </div>
+    <div class="col-sm-3" >    
+      <img  class='' src="../image/negotiations.svg" />
+    </div>
+  </div>
+  <div class="d-flex justify-content-center">
+    <button class="btn btn-lg col-s-12 bg-dark font-weight-bold text-light datepicker mt-5 text-center w-25 ">اشترك الآن</button>
+  </div>
+</section>
+<section class="text-right  servicesDiv d-flex justify-content-between pb-5">
+    <div class="col-6 divAboutUs">
+    <h3 class="font-weight-bold">ما الذي ستحصل عليه عند اشتراكك في بالعدل</h3>
+    <ul class="list-unstyled pt-3">
+        <li><img  class='pl-3' src="../image/consulting.svg" /> 
+        تقديم المشورة القانونية والمساعدة القانونية وجهاً لوجه مع المستشارين القانونيين في بالعدل في جميع أنجاء المملكة. 
+        </li>
+        <li><img  class='pl-3' src="../image/police.svg" /> 
+        تقديم الاستشارات القانونية لكل مسألة – وتحسم مستحقات قانونية جراء طلب تمثيل في القضايا المدنية والعمالة و الجنائية.
+        </li>
+        <li><img  class='pl-3' src="../image/courtServices.svg" /> 
+        التوجيه في المعاملات القضائية
+        </li>
+        <li><img  class='pl-3' src="../image/deptServices.svg" /> 
+        المساعدة في خدمات الديون عن طريق الاستشارات القانونية في حالات عديدة مثل: المفاوضات، التسوية الدمج، القوائم السوداء والإفلاس.
+        </li>
+        <li><img  class='pl-3' src="../image/serviceServices.svg" /> 
+        الدخول على شبكة بالعدل و توفير الخدمات القانونية. 
+        </li>
+        <li><img  class='pl-3' src="../image/GroupServices.svg" /> 
+        تقديم الاستشارات القانونية في مسائل العقود والمعاملات القانونية.
+        </li>  
+        <li><img  class='pl-3' src="../image/negotiation.svg" /> 
+        التواصل والمفاوضات مع أي أطراف ثالثة بحسب طلب المستخدم.
+        </li> 
+        <li><img  class='pl-3' src="../image/clockServices.svg" /> 
+        المساعدة القانونية على مدار 24 ساعة عبر خطوطنا القانونية المخصصة. 
+        </li>   
+    </ul>
+    </div>
+    <div class="col-5 divAboutUs">
+      <img  class='h-auto w-75' src="../image/photoServices.svg" />
+    </div>
+
+</section>
+
+<section class="d-flex flex-column  text-right p-5 servicesDiv" style="background-image:url('../image/backgroundServices.svg');  background-size: cover; height:auto">
+<h1>الخدمات المقدمة والغير مقدمة من بالعدل</h1>
+<div class="col-6 p-5">
+    <p>
+    من خلال مزايانا الممتدة، تسعى بالعدل إلى المساعدة في أي مسائل قانونية - حتى إذا لم تكن مغطاة بسياسة بالعدل الخاصة بك 
+    </p>
+</div>
+<div class="d-flex flex-row">
+    <div class="col-6">
+    <ul class="list-unstyled pt-3">
+        <li><img  class='pl-3' src="../image/true.svg" /> 
+        قضايا التأمين ( التأمين على الحياة أو السيارات )
+        </li>
+        <li><img  class='pl-3' src="../image/true.svg" /> 
+        المشكلات المتعلقة بالمستهلك (مثل الضمانات والسلع ذات العيوب)
+        </li>
+        <li><img  class='pl-3' src="../image/true.svg" /> 
+        مشكلات مالك العقار ( التعديلات او التحسينات او اعمال البناء)
+        </li>
+        <li><img  class='pl-3' src="../image/true.svg" /> 
+        القضايا العمالية &  القضايا الجنائية &  القضايا المرورية
+        </li>
+        <li><img  class='pl-3' src="../image/true.svg" /> 
+        خدمات الديون (على سبيل المثال ، مفاوضات التسوية ، تقديم المشورة بشأن الديون ، إلغاء أوامر الإدارة والأحكام).
+        </li>
+    </ul>
+    </div>
+    <div class="col-6">
+    <ul class="list-unstyled pt-3">
+        <li><img  class='pl-3' src="../image/false.svg" /> 
+        النزاعات الزوجية وحالات الطلاق والنفقة
+        </li>
+        <li><img  class='pl-3' src="../image/false.svg" /> 
+        التشهير والسب والقذف.
+        </li>
+        <li><img  class='pl-3' src="../image/false.svg" /> 
+        جميع القضايا والمسائل قبل انضمامك ل(بالعدل).
+        </li>
+        <li><img  class='pl-3' src="../image/false.svg" /> 
+        المخالفات المرورية.
+        </li>
+        <li><img  class='pl-3' src="../image/false.svg" /> 
+        القضايا السياسية والقضايا التجارية
+        </li>
+    </ul>
+    </div>
+</div>
+</section>
+
+<section class="text-right  servicesDiv d-flex justify-content-evenly p-5">
+<div class="col-5 divAboutUs">
+      <img  class='h-auto w-75' src="../image/photoServices.svg" />
+    </div>
+    <div class="col-6 divAboutUs">
+    <h3 class="font-weight-bold">كيف تقدم الخدمة؟</h3>
+    <ul class="list-unstyled pt-3">
+        <li><img  class='pl-3' src="../image/point05.svg" /> 
+        يتمتع الأعضاء بإمكانية الوصول المستمر إلى شبكة بالعدل الداخلية، والتي توفر المساعدة القانونية عبر الإنترنت أو عبر البريد الإلكتروني خلال ساعات العمل والمساعدة القانونية وجهاً لوجه عند الحاجة.
+        </li>
+        <li><img  class='pl-3' src="../image/point05.svg" /> 
+        فريقنا القانوني من بالعدل ذو الخبرة والتدريب. وهم قادرون على تقديم النصح لك، وصياغات الخطابات نيابة عنك ومحاولة حل القضايا القانونية دون اللجوء إلى المحكمة.
+        </li>
+        <li><img  class='pl-3' src="../image/point05.svg" /> 
+        سنقوم في بالعدل بإحالتك إلى محام عندما تكون لديك مشكلة قانونية تتطلب حضور الى المحكمة للترافع في الجلسات القضائية هيئات تسوية المنازعات.
+        </li>
+        <li><img  class='pl-3' src="../image/point05.svg" /> 
+        خطنا القانوني يقدم المساعدة القانونية عبر الهاتف 24 ساعة في اليوم ، 7 أيام في الأسبوع ، 365 يومًا في السنة.
+        </li>
+   
+    </ul>
+    </div>
+
+</section>
+
+
+
+
+<?php include('footer.php');?>
